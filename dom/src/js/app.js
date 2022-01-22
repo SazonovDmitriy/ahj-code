f0c0b93a930705951987dc11811f02e6d537c4e6
Dmitriy <sazonovd21@mail.ru>
2022-01-22 21:20:22 +0300

    v1

--- dom/src/js/app.js
+++ dom/src/js/app.js
@@ -1,1 +1,13 @@
-// TODO: write code here
+const fieldSize = 4;
+
+function board() {
+    const newBoard = [];
+
+    for (let i = 0; i < fieldSize; i += 1) {
+        newBoard[i] = [];
+        for (let j = 0; j < fieldSize; j += 1) {
+            newBoard[i][j] = '';
+        }
+    };
+    return newBoard;
+}
